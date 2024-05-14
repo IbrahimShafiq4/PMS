9c87a74f41a2313b76570fccf32613864a8618a4
IbrahimShafiq4 <hima.shafiq@gmail.com>
2024-05-15 01:31:54 +0300

    comments added in the shared-header

--- src/app/shared/components/shared-header/shared-header.component.ts
+++ src/app/shared/components/shared-header/shared-header.component.ts
@@ -2,15 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-shared-header',
-  templateUrl: './shared-header.component.html',
-  styleUrls: ['./shared-header.component.scss'],
+  templateUrl: './shared-header.component.html', // Template file path
+  styleUrls: ['./shared-header.component.scss'], // Styles file path
 })
 export class SharedHeaderComponent {
-  @Input() headerLinkText: string = '';
-  @Input() headerLink: string = '';
-  @Input() headerText: string = '';
-  @Input() headerMainText: string = '';
-  @Input() headerBtnText: string = '';
-  @Input() headerBtnVisibility: boolean = true;
-  @Input() headerBtnLink: string = '';
+  // Input properties, marked with @Input() decorator, to receive data from parent component
+  @Input() headerLinkText: string = ''; // Text for the link in the header
+  @Input() headerLink: string = ''; // Router link for the header link
+  @Input() headerText: string = ''; // Text for the main header
+  @Input() headerMainText: string = ''; // Additional main text for the header
+  @Input() headerBtnText: string = ''; // Text for the button in the header
+  @Input() headerBtnVisibility: boolean = true; // Visibility of the button
+  @Input() headerBtnLink: string = ''; // Router link for the button
 }
